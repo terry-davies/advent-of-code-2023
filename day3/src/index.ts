fcf8239c06bfa1b29e76c97db0ac38870a0af2d6
Terry Davies <terry.davies@yum.com>
2023-12-03 20:10:22 +0000

    feat: day 3: part 2 complete. so much refactoring possible

--- day3/src/index.ts
+++ day3/src/index.ts
@@ -6,6 +6,7 @@ type EngineMatrix = Array<Array<string>>;
 interface MatchResult {
   isMatch: boolean;
   number: string | null;
+  asterixDimension?: Array<string>;
 }
 
 function isNumeric(value: string) {
@@ -17,7 +18,11 @@ function isPeriodOrNumber(char: string | null) {
   return char === "." || isNumeric(char);
 }
 
-function getSiblingMatch(
+function isAsterix(char: string | null) {
+  return char === "*";
+}
+
+function getSiblingMatchDayOne(
   matrix: EngineMatrix,
   rowIndex: number,
   columnIndex: number,
@@ -26,19 +31,21 @@ function getSiblingMatch(
   const matrixWidth = matrix[rowIndex].length;
   const currentValue = matrix[rowIndex][columnIndex];
 
-  if (!isNumeric(currentValue)) {
-    return { isMatch: false, number: null };
-  }
-
-  if (isNumeric(currentValue) && alreadyMatched) {
-    return { isMatch: false, number: currentValue };
-  }
+  const isNumber = isNumeric(currentValue);
 
   let isMatch = false;
   let horizontal = columnIndex - 1;
   let vertical = rowIndex - 1;
 
+  let asterixDimension: Array<string> = [];
+
   while (!isMatch) {
+    try {
+      if (isAsterix(matrix[vertical][horizontal])) {
+        asterixDimension.push(`${vertical},${horizontal}`);
+      }
+    } catch (e) {}
+
     try {
       if (!isPeriodOrNumber(matrix[vertical][horizontal])) {
         isMatch = true;
@@ -66,7 +73,15 @@ function getSiblingMatch(
     }
   }
 
-  return { isMatch, number: currentValue };
+  if (!isNumber) {
+    return { isMatch: false, number: null };
+  }
+
+  if (isNumber && alreadyMatched) {
+    return { isMatch: false, number: currentValue, asterixDimension };
+  }
+
+  return { isMatch, number: currentValue, asterixDimension };
 }
 
 async function main() {
@@ -79,6 +94,8 @@ async function main() {
 
   let total = 0;
 
+  const matrixResults: Array<MatchResult> = [];
+
   const matrix: EngineMatrix = [];
 
   for await (const line of rl) {
@@ -88,23 +105,39 @@ async function main() {
   matrix.forEach((row, rowIndex) => {
     let currentNumber = null;
     let currentNumberMatch = false;
+    let asterixs: Array<string> = [];
 
     for (let i = 0; i < row.length; i++) {
-      const result = getSiblingMatch(matrix, rowIndex, i, currentNumberMatch);
+      const result = getSiblingMatchDayOne(
+        matrix,
+        rowIndex,
+        i,
+        currentNumberMatch
+      );
 
       if (result.isMatch) {
         currentNumberMatch = true;
       }
 
       if (result.number === null && !currentNumberMatch) {
+        matrixResults.push({ isMatch: false, number: currentNumber });
+
         currentNumber = null;
         continue;
       }
 
       if (result.number !== null && currentNumber === null) {
         currentNumber = result.number;
+
+        if (result.asterixDimension !== undefined) {
+          asterixs = [...new Set([...result.asterixDimension])];
+        }
       } else if (result.number !== null && currentNumber !== null) {
         currentNumber += result.number;
+
+        if (result.asterixDimension !== undefined) {
+          asterixs = [...new Set([...result.asterixDimension, ...asterixs])];
+        }
       }
 
       if (
@@ -112,14 +145,73 @@ async function main() {
         currentNumberMatch &&
         currentNumber !== null
       ) {
-        total += parseInt(currentNumber);
+        matrixResults.push({
+          isMatch: true,
+          number: currentNumber,
+          asterixDimension: asterixs,
+        });
+
+        asterixs = [];
         currentNumber = null;
         currentNumberMatch = false;
       }
     }
   });
 
-  console.log(`Total: ${total}`);
+  const asterixResult = matrixResults
+    .filter((matrixResult) => matrixResult.number !== null)
+    .reduce<Record<string, Array<string>>>((prev, current) => {
+      const currentNumber = current.number as string;
+      const value = current.asterixDimension;
+
+      if (value === undefined) return prev;
+
+      const mappedObject = current.asterixDimension?.reduce(
+        (mappedAsterixObject, asterixDimension) => {
+          const prevData = prev[asterixDimension];
+
+          if (!prevData) {
+            return {
+              [asterixDimension]: [currentNumber],
+            };
+          }
+
+          return {
+            ...mappedAsterixObject,
+            [asterixDimension]: [...prevData, currentNumber],
+          };
+        },
+        {} as Record<string, Array<string>>
+      );
+
+      return {
+        ...prev,
+        ...mappedObject,
+      } as Record<string, Array<string>>;
+    }, {});
+
+  const part2Total = Object.keys(asterixResult).reduce((total, current) => {
+    if (asterixResult[current].length === 2) {
+      return (
+        total +
+        Number(asterixResult[current][0]) * Number(asterixResult[current][1])
+      );
+    }
+
+    return total;
+  }, 0);
+
+  console.log(
+    `Total: ${matrixResults.reduce<number | undefined>((prev, current) => {
+      if (prev === undefined) return 0;
+
+      if (!current.isMatch) return prev;
+
+      return prev + Number(current.number);
+    }, 0)}`
+  );
+
+  console.log(`Part 2 Total: ${part2Total}`);
 }
 
 main();
