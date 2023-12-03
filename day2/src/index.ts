7e7292188cf0b65f4b1e3737d239bfb77f6b69c8
Terry Davies <terry.davies@yum.com>
2023-12-03 14:09:24 +0000

    feat: day 2: first solution

--- day2/src/index.ts
+++ day2/src/index.ts
@@ -1,5 +1,149 @@
+import fs from "fs";
+import readline from "readline";
+
+const numberRegex = /\d+/g;
+
+function getGameNumber(gameStr: string) {
+  const gameNumber = gameStr.match(numberRegex) as string[];
+
+  if (gameNumber.length > 1) {
+    throw new Error("Invalid game");
+  }
+
+  return parseInt(gameNumber[0]);
+}
+
+interface RuleSet {
+  redMax: number | null;
+  greenMax: number | null;
+  blueMax: number | null;
+}
+
+const defaultConfig: RuleSet = {
+  redMax: 12,
+  greenMax: 13,
+  blueMax: 14,
+};
+
+const colours = {
+  blue: "blue",
+  red: "red",
+  green: "green",
+} as const;
+
+function handleColour(
+  strResult: string,
+  colour: (typeof colours)[keyof typeof colours]
+) {
+  const result = strResult.replace(colour, "").trim();
+  return parseInt(result);
+}
+
+function handleRound(result: string, currentState: RuleSet) {
+  let returnState = { ...currentState };
+
+  if (result.includes("blue")) {
+    const total = handleColour(result, "blue");
+
+    returnState.blueMax =
+      returnState.blueMax === null || total > returnState.blueMax
+        ? total
+        : currentState.blueMax;
+  } else if (result.includes("red")) {
+    const total = handleColour(result, "red");
+    returnState.redMax =
+      returnState.redMax === null || total > returnState.redMax
+        ? total
+        : currentState.redMax;
+  } else if (result.includes("green")) {
+    const total = handleColour(result, "green");
+    returnState.greenMax =
+      returnState.greenMax === null || total > returnState.greenMax
+        ? total
+        : currentState.greenMax;
+  }
+
+  return returnState;
+}
+
+function handleLine(line: string) {
+  const [game, result] = line.split(":");
+  const gameNumber = getGameNumber(game);
+
+  const rounds = result.split(";");
+
+  const roundResult = rounds.reduce<RuleSet>(
+    (prev, currentRound) => {
+      const roundResults = currentRound.split(",");
+
+      console.log(prev);
+
+      roundResults.forEach((result) => {
+        prev = handleRound(result, prev);
+      });
+
+      return prev;
+    },
+    {
+      blueMax: null,
+      redMax: null,
+      greenMax: null,
+    } as RuleSet
+  );
+
+  console.log(roundResult);
+
+  return { ...roundResult, gameNumber };
+}
+
+function isGamePossible(
+  result: ReturnType<typeof handleLine>,
+  rules: typeof defaultConfig = defaultConfig
+) {
+  const { greenMax, blueMax, redMax } = result;
+
+  if (greenMax === null || blueMax === null || redMax === null) {
+    return false;
+  }
+
+  if (
+    rules.greenMax === null ||
+    rules.blueMax === null ||
+    rules.redMax === null
+  ) {
+    return false;
+  }
+
+  if (
+    greenMax > rules.greenMax ||
+    blueMax > rules.blueMax ||
+    redMax > rules.redMax
+  ) {
+    return false;
+  }
+
+  return true;
+}
+
 async function main() {
   console.log("start");
+
+  const fileStream = fs.createReadStream("./src/input.txt");
+
+  const rl = readline.createInterface({
+    input: fileStream,
+    crlfDelay: Infinity,
+  });
+
+  let total = 0;
+
+  for await (const line of rl) {
+    const result = handleLine(line);
+    console.log(result);
+    if (isGamePossible(result)) total += result.gameNumber;
+  }
+
+  console.log(`Total: ${total}`);
 }
 
 main();
