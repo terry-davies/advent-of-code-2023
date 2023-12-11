773071275b93def19fad2d2a4bd28a9f907fdc99
Terry Davies <terry.davies@yum.com>
2023-12-11 14:22:38 +0000

    feat: day 10: part 1 solution

---
+++ day10/src/index.ts
@@ -0,0 +1,203 @@
+import fs from "fs";
+
+const input = fs.readFileSync("./src/input.txt", "utf8").replace(/\r/g, "");
+
+type Grid = string[][];
+
+type Cell = [number, number];
+
+type MapperFunction = (
+  cell: Cell,
+  direction: Direction
+) => Cell | null | undefined;
+
+type CellMapper = Record<string, MapperFunction>;
+type DirectionMapper = Record<string, Partial<Record<Direction, Direction>>>;
+
+enum Direction {
+  North,
+  East,
+  South,
+  West,
+}
+
+interface MoveData {
+  direction: Direction;
+  cell: Cell;
+  value: string;
+}
+
+const moveNorth = (cell: Cell): Cell => [cell[0] - 1, cell[1]];
+const moveEast = (cell: Cell): Cell => [cell[0], cell[1] + 1];
+const moveSouth = (cell: Cell): Cell => [cell[0] + 1, cell[1]];
+const moveWest = (cell: Cell): Cell => [cell[0], cell[1] - 1];
+
+const cellMapper: CellMapper = {
+  "|": (cell: Cell, direction: Direction) => {
+    if (direction === Direction.North) return moveNorth(cell);
+    if (direction === Direction.South) return moveSouth(cell);
+  },
+  "-": (cell: Cell, direction: Direction) => {
+    if (direction === Direction.East) return moveEast(cell);
+    if (direction === Direction.West) return moveWest(cell);
+  },
+  J: (cell: Cell, direction: Direction) => {
+    if (direction === Direction.South) return moveWest(cell);
+    if (direction === Direction.East) return moveNorth(cell);
+  }, //is a 90-degree bend connecting north and west.
+  "7": (cell: Cell, direction: Direction) => {
+    if (direction === Direction.East) return moveSouth(cell);
+    if (direction === Direction.North) return moveNorth(cell);
+  }, //is a 90-degree bend connecting south and west.
+  F: (cell: Cell, direction: Direction) => {
+    if (direction === Direction.South) return moveSouth(cell);
+    if (direction === Direction.West) return moveWest(cell);
+  }, //is a 90-degree bend connecting south and east.
+  L: (cell: Cell, direction: Direction) => {
+    console.log(cell, direction);
+    if (direction === Direction.East) return moveEast(cell);
+    if (direction === Direction.South) return moveSouth(cell);
+  }, // L is a 90-degree bend connecting north and east.
+  ".": (cell: Cell, direction: Direction) => {
+    return null;
+  },
+};
+
+const directionMapper: DirectionMapper = {
+  "|": {
+    [Direction.North]: Direction.North,
+    [Direction.South]: Direction.South,
+  },
+  "-": {
+    [Direction.East]: Direction.East,
+    [Direction.West]: Direction.West,
+  },
+  J: {
+    [Direction.South]: Direction.West,
+    [Direction.East]: Direction.North,
+  }, //is a 90-degree bend connecting north and west.
+  "7": {
+    [Direction.North]: Direction.West,
+    [Direction.East]: Direction.South,
+  }, //is a 90-degree bend connecting south and west.
+  F: {
+    [Direction.North]: Direction.East,
+    [Direction.West]: Direction.South,
+  }, //is a 90-degree bend connecting south and east.
+  L: {
+    [Direction.South]: Direction.East,
+    [Direction.West]: Direction.North,
+  }, // L is a 90-degree bend connecting north and east.
+};
+
+type DirectionMapperKey = keyof typeof cellMapper;
+
+function traverseGrid(startCell: Cell, grid: Grid) {
+  const firstCellData = getFirstCell(startCell, grid);
+
+  if (!firstCellData) throw new Error("no starting cell");
+
+  let steps = 0;
+
+  let currentDirection = firstCellData.direction;
+  let currentCell = firstCellData.cell;
+  let currentCellValue = firstCellData.value;
+
+  while (currentCellValue !== "S") {
+    if (currentDirection === Direction.North) {
+      currentCell = moveNorth(currentCell);
+    } else if (currentDirection === Direction.East) {
+      currentCell = moveEast(currentCell);
+    } else if (currentDirection === Direction.South) {
+      currentCell = moveSouth(currentCell);
+    } else if (currentDirection === Direction.West) {
+      currentCell = moveWest(currentCell);
+    }
+
+    currentCellValue = grid[currentCell[0]][currentCell[1]];
+
+    if (currentCellValue === "S") {
+      steps++;
+      break;
+    }
+
+    const newDirection = directionMapper[currentCellValue][currentDirection];
+    if (newDirection === undefined) throw new Error("no direction");
+
+    currentDirection = newDirection;
+
+    console.log("Direction", currentDirection);
+    console.log("Cell", currentCell);
+    console.log("Value", currentCellValue);
+
+    steps++;
+  }
+
+  return steps;
+}
+
+function getFirstCell(cell: Cell, grid: Grid) {
+  const adjacentCells = getAdjacentCellValues(cell, grid);
+
+  return adjacentCells
+    .reduce<MoveData[]>((acc, curr, index) => {
+      if (curr === undefined) return acc;
+
+      const direction =
+        index === 0
+          ? Direction.North
+          : index === 1
+          ? Direction.East
+          : index === 2
+          ? Direction.South
+          : Direction.West;
+
+      return [
+        ...acc,
+        {
+          direction,
+          value: curr,
+          cell,
+        },
+      ];
+    }, [])
+    .pop();
+}
+
+function parseInput(input: string): Grid {
+  return input.split("\n").map((line) => {
+    return line.split("");
+  });
+}
+
+function findStartCell(grid: Grid): Cell {
+  for (let i = 0; i < grid.length; i++) {
+    for (let j = 0; j < grid[i].length; j++) {
+      if (grid[i][j] === "S") return [i, j];
+    }
+  }
+
+  throw new Error("No S in grid");
+}
+
+function getAdjacentCellValues(cell: Cell, grid: Grid) {
+  const [x, y] = cell;
+
+  const gridHeight = grid.length;
+  const gridWidth = grid[0].length;
+
+  const northCell = x - 1 >= 0 ? grid[x - 1][y] : undefined;
+  const eastCell = y + 1 < gridWidth ? grid[x][y + 1] : undefined;
+  const southCell = x + 1 < gridHeight ? grid[x + 1][y] : undefined;
+  const westCell = y - 1 >= 0 ? grid[x][y - 1] : undefined;
+
+  return [northCell, eastCell, southCell, westCell].map((cell) => {
+    if (cell === "." || cell === "S") return undefined;
+    return cell;
+  });
+}
+
+const grid = parseInput(input);
+const startCell = findStartCell(grid);
+
+console.log(traverseGrid(startCell, grid) / 2);
