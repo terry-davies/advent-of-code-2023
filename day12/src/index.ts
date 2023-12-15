5fa9d2532a6c1a5b7ed3d223e410d21a901b0399
Terry Davies <terry.davies@yum.com>
2023-12-15 23:16:29 +0000

    feat: day 12: part 2 solution with generic memoize function

--- day12/src/index.ts
+++ day12/src/index.ts
@@ -9,6 +9,27 @@ interface RowData {
   springs: string;
 }
 
+function memoize<Args extends unknown[], Result>(
+  func: (...args: Args) => Result
+): (...args: Args) => Result {
+  const stored = new Map<string, Result>();
+
+  return (...args) => {
+    const k = JSON.stringify(args);
+
+    if (stored.has(k)) {
+      return stored.get(k)!;
+    }
+    const result = func(...args);
+    stored.set(k, result);
+    return result;
+  };
+}
+
+function createArray(length: number, start = 0) {
+  return Array.from({ length }, (_, i) => i + start + 1);
+}
+
 function parseInput(input: string): RowData[] {
   return input.split("\n").map((line) => {
     const [springs, arrangements] = line.split(" ");
@@ -28,50 +49,73 @@ function sum(nums: number[]): number {
   }, 0);
 }
 
-function handleLine(springs: string, arrangements: Arrangements): number {
-  if (springs.length === 0) {
-    return arrangements.length === 0 ? 1 : 0;
-  }
-
-  if (arrangements.length === 0) {
-    return springs.includes("#") ? 0 : 1;
-  }
-  //console.log("here");
-
-  if (springs.length < sum(arrangements)) {
-    return 0;
-  }
-
-  if (springs.startsWith(".")) {
-    return handleLine(springs.slice(1), arrangements);
-  }
-
-  if (springs.startsWith("#")) {
-    const [currentRun, ...leftoverRuns] = arrangements;
-    for (let i = 0; i < currentRun; i++) {
-      if (springs[i] === ".") {
-        return 0;
-      }
+const handleLine = memoize(
+  (springs: string, arrangements: Arrangements): number => {
+    if (springs.length === 0) {
+      return arrangements.length === 0 ? 1 : 0;
     }
-    if (springs[currentRun] === "#") {
+
+    if (arrangements.length === 0) {
+      return springs.includes("#") ? 0 : 1;
+    }
+
+    if (springs.length < sum(arrangements)) {
       return 0;
     }
 
-    return handleLine(springs.slice(currentRun + 1), leftoverRuns);
-  }
+    if (springs.startsWith(".")) {
+      return handleLine(springs.slice(1), arrangements);
+    }
 
-  return (
-    handleLine("#" + springs.slice(1), arrangements) +
-    handleLine("." + springs.slice(1), arrangements)
-  );
-}
+    if (springs.startsWith("#")) {
+      const [currentRun, ...leftoverRuns] = arrangements;
+      for (let i = 0; i < currentRun; i++) {
+        if (springs[i] === ".") {
+          return 0;
+        }
+      }
+      if (springs[currentRun] === "#") {
+        return 0;
+      }
+
+      return handleLine(springs.slice(currentRun + 1), leftoverRuns);
+    }
+
+    return (
+      handleLine("#" + springs.slice(1), arrangements) +
+      handleLine("." + springs.slice(1), arrangements)
+    );
+  }
+);
 
 const parsed = parseInput(input);
 
 console.log(
   parsed.reduce((acc, current) => {
     const combos = handleLine(current.springs, current.arrangements);
-    console.log(current.springs, combos);
+
+    return acc + combos;
+  }, 0)
+);
+
+console.log(
+  parsed.reduce((acc, current) => {
+    const { springs, arrangements } = current;
+
+    const skeletonArray = createArray(5);
+
+    const part2Springs = skeletonArray
+      .map((_) => {
+        return springs;
+      })
+      .join("?");
+
+    const part2Arrangments = skeletonArray.reduce<number[]>((acc, current) => {
+      return [...acc, ...arrangements];
+    }, []);
+
+    const combos = handleLine(part2Springs, part2Arrangments);
+
     return acc + combos;
   }, 0)
 );
