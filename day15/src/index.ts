e437df5cfacfd54a2c70706191445af1de948b4a
Terry Davies <terry.davies@yum.com>
2023-12-17 20:54:16 +0000

    feat: day 15: part 2 solution

--- day15/src/index.ts
+++ day15/src/index.ts
@@ -2,11 +2,28 @@ import fs from "fs";
 
 const input = fs.readFileSync("./src/input.txt", "utf8").replace(/\r/g, "");
 
+function memoize<Args extends unknown[], Result>(
+  func: (...args: Args) => Result
+): (...args: Args) => Result {
+  const stored = new Map<string, Result>();
+
+  return (...args) => {
+    const k = JSON.stringify(args);
+
+    if (stored.has(k)) {
+      return stored.get(k)!;
+    }
+    const result = func(...args);
+    stored.set(k, result);
+    return result;
+  };
+}
+
 function parseInput(input: string) {
   return input.split(",");
 }
 
-function calculateStepValue(step: string) {
+const calculateStepValue = memoize((step: string) => {
   let total = 0;
 
   for (let i = 0; i < step.length; i++) {
@@ -17,9 +34,9 @@ function calculateStepValue(step: string) {
   }
 
   return total;
-}
+});
 
-function processSteps(steps: string[]) {
+function processStepsPart1(steps: string[]) {
   const stepValues = steps.reduce((acc, curr) => {
     const value = calculateStepValue(curr);
 
@@ -28,4 +45,83 @@ function processSteps(steps: string[]) {
   return stepValues;
 }
 
-console.log(processSteps(parseInput(input)));
+function calculateBoxValues(box: Map<number, Array<string>>) {
+  let total = 0;
+
+  box.forEach((value, key) => {
+    const boxNumber = key + 1;
+    const boxValue = value.reduce((acc, curr, index) => {
+      const slotNumber = index + 1;
+      const focalLength = Number(curr.split(" ")[1]);
+      return acc + boxNumber * slotNumber * focalLength;
+    }, 0);
+
+    total += boxValue;
+  });
+
+  return total;
+}
+
+function processStepsPart2(steps: string[]) {
+  const boxMap = new Map<number, Array<string>>();
+
+  steps.forEach((step) => {
+    let lensCode = "";
+    let instruction: "-" | "=" = "-";
+    let lensCodeValue: undefined | string;
+
+    if (step.includes("=")) {
+      instruction = "=";
+      const parts = step.split("=");
+      lensCode = parts[0];
+      lensCodeValue = parts[1];
+    } else {
+      const parts = step.split("-");
+      lensCode = parts[0];
+    }
+
+    const value = calculateStepValue(lensCode.split(" ")[0]);
+
+    if (instruction === "-") {
+      const currentBox = boxMap.get(value);
+
+      if (currentBox !== undefined) {
+        const updatedBox = [...currentBox].filter(
+          (value) => !value.includes(lensCode)
+        );
+        boxMap.set(value, updatedBox);
+      }
+    }
+
+    if (instruction === "=") {
+      const currentBox = boxMap.get(value);
+
+      if (currentBox !== undefined) {
+        const boxContents = [...currentBox];
+
+        const isLensInBox = boxContents.find((value) =>
+          value.includes(lensCode)
+        );
+
+        if (isLensInBox) {
+          const indexOfFound = boxContents.findIndex((value) =>
+            value.includes(lensCode)
+          );
+
+          boxContents[indexOfFound] = `${lensCode} ${lensCodeValue}`;
+          boxMap.set(value, boxContents);
+        } else {
+          boxContents.push(`${lensCode} ${lensCodeValue}`);
+          boxMap.set(value, boxContents);
+        }
+      } else {
+        boxMap.set(value, [`${lensCode} ${lensCodeValue}`]);
+      }
+    }
+  });
+
+  return boxMap;
+}
+
+console.log(processStepsPart1(parseInput(input)));
+console.log(calculateBoxValues(processStepsPart2(parseInput(input))));
